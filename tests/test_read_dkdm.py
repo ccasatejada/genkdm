9828bc897f23135dd5d428c4e4f36caf1b8e2dcc
clement <clement.casatejada@cinego.net>
2025-04-09 18:29:07 +0200

    dkdm name correction

--- tests/test_read_dkdm.py
+++ tests/test_read_dkdm.py
@@ -9,5 +9,5 @@ def test_parse_xml():
     test that read_dkdm.parse_xml do not throw any exception and return list
     :return:
     """
-    mains = read_dkdm.parse_xml('../files/dkdm/DKDM_Pachamama_FTR-1_F_FR-XX_FR_51-VI_2K_FOLI_20181113_HVY_IOP_OV.xml')
+    mains = read_dkdm.parse_xml('../files/dkdm/any_dkdm_file.xml')
     assert len(mains) == 3, "function must return list with 3 items"
